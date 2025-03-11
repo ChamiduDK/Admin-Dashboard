7971b51852f9e2c1c0186d6f3643f31406fc237e
ChamiduDK <chamidudhilshan@gmail.com>
2025-03-11 13:10:03 +0530

    Add database connection script

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+<?php
+// Database credentials
+$host = '';             
+$username = '';  
+$password = '';          
+$dbname = '';   
+
+// Create connection
+$connection = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+?>
